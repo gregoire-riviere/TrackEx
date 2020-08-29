0cb1d97dfaeba073dfac0608f0426b3d8b23b71d
Grégoire <gregoire.riviere@kbrw.fr>
2020-08-29 12:18:23 +0200

    First commit!

---
+++ lib/trackex.ex
@@ -0,0 +1,18 @@
+defmodule Trackex do
+  @moduledoc """
+  Documentation for Trackex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Trackex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/trackex_test.exs
@@ -0,0 +1,8 @@
+defmodule TrackexTest do
+  use ExUnit.Case
+  doctest Trackex
+
+  test "greets the world" do
+    assert Trackex.hello() == :world
+  end
+end
